9d6250c6e17be80a0a6f6d87e171298295b0523d
ThienLan <meomeocf98@gmail.com>
2019-04-15 01:20:29 +0700

    Add route

--- playing-typescript/src/app/hero-detail/hero-detail.component.ts
+++ playing-typescript/src/app/hero-detail/hero-detail.component.ts
@@ -1,17 +1,40 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Location } from "@angular/common";
+
+import { HeroService } from "../hero.service";
 import { Hero } from 'src/models/hero';
 
 @Component({
-  selector: 'app-hero-detail',
-  templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  selector: "app-hero-detail",
+  templateUrl: "./hero-detail.component.html",
+  styleUrls: ["./hero-detail.component.css"]
 })
 export class HeroDetailComponent implements OnInit {
+  hero: Hero;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.getHero();
   }
-  @Input() hero: Hero;
-  
+
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get("id");
+    this.heroService.getHero(id).subscribe(hero => (this.hero = hero));
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
--- playing-typescript/src/app/heroes/heroes.component.ts
+++ playing-typescript/src/app/heroes/heroes.component.ts
@@ -1,28 +1,23 @@
 import { Component, OnInit } from "@angular/core";
-import { Hero } from "src/models/hero";
+
 import { HeroService } from "../hero.service";
+import { Hero } from 'src/models/hero';
+
 @Component({
   selector: "app-heroes",
   templateUrl: "./heroes.component.html",
-  styleUrls: ["./heroes.component.css", "../app.component.css"]
+  styleUrls: ["./heroes.component.css"]
 })
 export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+
   constructor(private heroService: HeroService) {}
 
   ngOnInit() {
     this.getHeroes();
   }
-  heroes: Hero[];
-  hero: Hero = {
-    id: 1,
-    name: "Meo Meo"
-  };
-  selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 
   getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+    this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
   }
 }
